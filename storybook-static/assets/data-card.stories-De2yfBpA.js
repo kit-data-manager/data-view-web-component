e2380c7ae596835c0ec4377c762cca8fcfe3fd58
Thomas Jejkal <thomas.jejkal@kit.edu>
2025-02-21 12:33:05 +0100

    Try to fix storybook issues, updated version number

---
+++ storybook-static/assets/data-card.stories-De2yfBpA.js
@@ -0,0 +1,95 @@
+const T="Vatikan Vat Gr 247",w="SFB 980 - A04, 2019",N="",O={value:"2019-03-11T14:13:45Z",label:"Created at"},R="http://episteme2.scc.kit.edu:8080/api/v1/dataresources/3f3bf25b-e0b9-48a9-b344-20630f733f8b/data/076r.thumb.jpg",J="",U=[{url:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/000073cd-c425-4214-9648-b380ff20c61a",label:"DataCite"},{url:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/000073cd-c425-4214-9648-b380ff20c61a/data/manuscript_metadata.xml",label:"Metadata",position:"metadata-container"},{url:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/000073cd-c425-4214-9648-b380ff20c61a/data/pages.json",label:"Page Information"}],k=[{text:"Manuscript",color:"blue"},{text:"SFB 980 - A04",color:"blue",url:"..."},{text:"Open",color:"green",icon:"lock"}],j=[{dataTitle:"Vatikan Vat Gr 247, 084r",subtitle:"SFB 980 - A04, 2019",imageURL:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.thumb.jpg",textRight:{value:"2019-03-11T14:13:44Z",label:"Created at"},downloads:[{url:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090",label:"DataCite"}],childrenData:[{dataTitle:"084r.master.jpg",subtitle:"SFB 980 - A04, 2019",downloadUrl:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.master.jpg",imageURL:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.thumb.jpg",textRight:{value:"image/jpeg",label:"MediaType"},tags:[{text:"Master",color:"blue"}],metadata:[{label:"Checksum",value:"sha1:3ffa407982d4aa653f8dcf2232ac81df15124abb"},{label:"Size",value:"8678356"},{label:"Resolution (x)",value:"400"},{label:"Resolution (y)",value:"400"},{label:"Width",value:"3307px"},{label:"Height",value:"4677px"}]},{dataTitle:"084r.thumb.jpg",subtitle:"SFB 980 - A04, 2019",downloadUrl:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.thumb.jpg",imageURL:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.thumb.jpg",textRight:{value:"image/jpeg",label:"MediaType"},tags:[{text:"Thumb",color:"blue"}],metadata:[{label:"Checksum",value:"sha1:6138a07ff149326e51b22bb74e26c17cf13b0e18"},{label:"Size",value:"7008"},{label:"Resolution (x)",value:"400"},{label:"Resolution (y)",value:"400"},{label:"Width",value:"106px"},{label:"Height",value:"150px"}]},{dataTitle:"084r.tif",subtitle:"SFB 980 - A04, 2019",downloadUrl:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.tif",imageURL:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.thumb.jpg",textRight:{value:"image/jpeg",label:"MediaType"},tags:[{text:"Original",color:"blue"}],metadata:[{label:"Checksum",value:"sha1:da5e6acf67dbb56d31900f22d9db73dfa5cae312"},{label:"Size",value:"15467351"},{label:"Resolution (x)",value:"400"},{label:"Resolution (y)",value:"400"},{label:"Width",value:"3307px"},{label:"Height",value:"4677px"}]}]}],D=[{label:"Location",value:"Biblioteca Apostolica Vaticana"},{label:"Location",value:"Vatikan"},{label:"Pages",value:"18"}],$={dataTitle:T,subTitle:w,bodyText:N,textRight:O,imageUrl:R,downloadURL:J,downloads:U,tags:k,childrenData:j,metadata:D},B={title:"Data Card",args:{imageUrl:"https://upload.wikimedia.org/wikipedia/commons/b/b6/Gutenberg_Bible%2C_Lenox_Copy%2C_New_York_Public_Library%2C_2009._Pic_01.jpg",dataTitle:"A sample resource",subTitle:"KIT, 2020",bodyText:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor.",textRight:{label:"Date updated",value:new Date().toLocaleDateString()},tags:[{text:"A tag",color:"green"},{text:"Another tag",color:"blue",iconName:"ph:lock-simple-fill"}],metadata:[{label:"Publisher",value:"KIT"},{label:"Publication Year",value:"2020"},{label:"Resource Type",value:"Dataset"},{label:"Resource Format",value:"CSV"},{label:"Created at",value:"2024-01-01 00:00:00"},{label:"Last modified",value:"2024-01-01 00:00:00"},{label:"License",value:"Apache 2.0"},{label:"Related identifiers",value:"None"},{label:"Alternate identifiers",value:"211fae3e-4f7e-4c3e-8f3a-9f4c9c5c9c9c",url:"http://localhost"}],variant:"default",childrenData:[{dataTitle:"Vatikan Vat Gr 247, 084r",subtitle:"SFB 980 - A04, 2019",imageURL:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.thumb.jpg",textRight:{value:"2019-03-11T14:13:44Z",label:"Created at"},actionButtons:[{iconName:"ph:download-simple-light",url:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090",label:"DataCite"}],childrenData:[{dataTitle:"084r.master.jpg",subtitle:"SFB 980 - A04, 2019",actionButtons:[{iconName:"ph:download-simple-light",label:"Download",url:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.master.jpg"},{iconName:"ph:trash",label:"Delete",url:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.master.jpg"}],imageURL:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.thumb.jpg",textRight:{value:"image/jpeg",label:"MediaType"},tags:[{text:"Master",color:"blue"}],metadata:[{label:"Checksum",value:"sha1:3ffa407982d4aa653f8dcf2232ac81df15124abb"},{label:"Size",value:"8678356"},{label:"Resolution (x)",value:"400"},{label:"Resolution (y)",value:"400"},{label:"Width",value:"3307px"},{label:"Height",value:"4677px"}]},{dataTitle:"084r.thumb.jpg",subtitle:"SFB 980 - A04, 2019",actionButtons:[{iconName:"ph:download-simple-light",label:"Download",url:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.thumb.jpg"}],imageURL:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.thumb.jpg",textRight:{value:"image/jpeg",label:"MediaType"},tags:[{text:"Thumb",color:"blue"}],metadata:[{label:"Checksum",value:"sha1:6138a07ff149326e51b22bb74e26c17cf13b0e18"},{label:"Size",value:"7008"},{label:"Resolution (x)",value:"400"},{label:"Resolution (y)",value:"400"},{label:"Width",value:"106px"},{label:"Height",value:"150px"}]},{dataTitle:"084r.tif",subtitle:"SFB 980 - A04, 2019",actionButtons:[{iconName:"ph:download-simple-light",label:"Download",url:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.tif"}],imageURL:"http://episteme2.scc.kit.edu:8080/api/v1/dataresources/85ac585d-da66-4f9e-bc5c-f55851a1c090/data/084r.thumb.jpg",textRight:{value:"image/jpeg",label:"MediaType"},tags:[{text:"Original",color:"blue"}],metadata:[{label:"Checksum",value:"sha1:da5e6acf67dbb56d31900f22d9db73dfa5cae312"},{label:"Size",value:"15467351"},{label:"Resolution (x)",value:"400"},{label:"Resolution (y)",value:"400"},{label:"Width",value:"3307px"},{label:"Height",value:"4677px"}]}]}],downloadUrl:"test",actionButtons:[{iconName:"ph:download-simple-light",label:"Download",url:"https://www.google.com"},{iconName:"ph:pencil",label:"Edit",eventIdentifier:"edit-sample"},{iconName:"ph:download-fill",url:"https://www.google.com",position:"metadata-container"},{iconName:"ph:share-network-fill",label:"Share Metadata",url:"https://www.google.com",position:"metadata-container"}],editUrl:"test",downloads:[]},argTypes:{imageUrl:{control:"text"},dataTitle:{control:"text",defaultValue:"A sample resource"},subTitle:{control:"text"},bodyText:{control:"text"},textRight:{control:"object"},tags:{control:"object"},metadata:{control:"object"},childrenData:{control:"object"},downloadUrl:{control:"text"},downloads:{control:"object"},variant:{control:"radio",options:["default","detailed","minimal"]}}},r=a=>`<data-card
+    image-url="${a.imageUrl}"
+    data-title='${a.dataTitle}'
+    sub-title="${a.subTitle}"
+    body-text="${a.bodyText}"
+    text-right='${JSON.stringify(a.textRight)}'
+    children-data='${JSON.stringify(a.childrenData)}'
+    action-buttons='${JSON.stringify(a.actionButtons)}'
+    tags='${JSON.stringify(a.tags)}'
+    download-url="${a.downloadUrl}"
+    edit-url="${a.editUrl}"
+    downloads='${JSON.stringify(a.downloads)}'
+    metadata='${JSON.stringify(a.metadata)}'
+    variant="${a.variant}"
+  ></data-card>`,s=r.bind({}),t=r.bind({});t.args={...t.args,variant:"minimal"};const e=r.bind({});e.args={...e.args,variant:"detailed"};const i=r.bind({});i.args={...$};const l=r.bind({});l.args={...$,variant:"detailed"};var d,n,o;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`(args: Story['args']) => \`<data-card
+    image-url="\${args.imageUrl}"
+    data-title='\${
+// args.dataTitleType === 'string' ? args.dataTitleString : JSON.stringify(args.dataTitleObject)
+args.dataTitle}'
+    sub-title="\${args.subTitle}"
+    body-text="\${args.bodyText}"
+    text-right='\${JSON.stringify(args.textRight)}'
+    children-data='\${JSON.stringify(args.childrenData)}'
+    action-buttons='\${JSON.stringify(args.actionButtons)}'
+    tags='\${JSON.stringify(args.tags)}'
+    download-url="\${args.downloadUrl}"
+    edit-url="\${args.editUrl}"
+    downloads='\${JSON.stringify(args.downloads)}'
+    metadata='\${JSON.stringify(args.metadata)}'
+    variant="\${args.variant}"
+  ></data-card>\``,...(o=(n=s.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};var c,g,u;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`(args: Story['args']) => \`<data-card
+    image-url="\${args.imageUrl}"
+    data-title='\${
+// args.dataTitleType === 'string' ? args.dataTitleString : JSON.stringify(args.dataTitleObject)
+args.dataTitle}'
+    sub-title="\${args.subTitle}"
+    body-text="\${args.bodyText}"
+    text-right='\${JSON.stringify(args.textRight)}'
+    children-data='\${JSON.stringify(args.childrenData)}'
+    action-buttons='\${JSON.stringify(args.actionButtons)}'
+    tags='\${JSON.stringify(args.tags)}'
+    download-url="\${args.downloadUrl}"
+    edit-url="\${args.editUrl}"
+    downloads='\${JSON.stringify(args.downloads)}'
+    metadata='\${JSON.stringify(args.metadata)}'
+    variant="\${args.variant}"
+  ></data-card>\``,...(u=(g=t.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var b,p,m;e.parameters={...e.parameters,docs:{...(b=e.parameters)==null?void 0:b.docs,source:{originalSource:`(args: Story['args']) => \`<data-card
+    image-url="\${args.imageUrl}"
+    data-title='\${
+// args.dataTitleType === 'string' ? args.dataTitleString : JSON.stringify(args.dataTitleObject)
+args.dataTitle}'
+    sub-title="\${args.subTitle}"
+    body-text="\${args.bodyText}"
+    text-right='\${JSON.stringify(args.textRight)}'
+    children-data='\${JSON.stringify(args.childrenData)}'
+    action-buttons='\${JSON.stringify(args.actionButtons)}'
+    tags='\${JSON.stringify(args.tags)}'
+    download-url="\${args.downloadUrl}"
+    edit-url="\${args.editUrl}"
+    downloads='\${JSON.stringify(args.downloads)}'
+    metadata='\${JSON.stringify(args.metadata)}'
+    variant="\${args.variant}"
+  ></data-card>\``,...(m=(p=e.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var h,f,v;i.parameters={...i.parameters,docs:{...(h=i.parameters)==null?void 0:h.docs,source:{originalSource:`(args: Story['args']) => \`<data-card
+    image-url="\${args.imageUrl}"
+    data-title='\${
+// args.dataTitleType === 'string' ? args.dataTitleString : JSON.stringify(args.dataTitleObject)
+args.dataTitle}'
+    sub-title="\${args.subTitle}"
+    body-text="\${args.bodyText}"
+    text-right='\${JSON.stringify(args.textRight)}'
+    children-data='\${JSON.stringify(args.childrenData)}'
+    action-buttons='\${JSON.stringify(args.actionButtons)}'
+    tags='\${JSON.stringify(args.tags)}'
+    download-url="\${args.downloadUrl}"
+    edit-url="\${args.editUrl}"
+    downloads='\${JSON.stringify(args.downloads)}'
+    metadata='\${JSON.stringify(args.metadata)}'
+    variant="\${args.variant}"
+  ></data-card>\``,...(v=(f=i.parameters)==null?void 0:f.docs)==null?void 0:v.source}}};var y,S,x;l.parameters={...l.parameters,docs:{...(y=l.parameters)==null?void 0:y.docs,source:{originalSource:`(args: Story['args']) => \`<data-card
+    image-url="\${args.imageUrl}"
+    data-title='\${
+// args.dataTitleType === 'string' ? args.dataTitleString : JSON.stringify(args.dataTitleObject)
+args.dataTitle}'
+    sub-title="\${args.subTitle}"
+    body-text="\${args.bodyText}"
+    text-right='\${JSON.stringify(args.textRight)}'
+    children-data='\${JSON.stringify(args.childrenData)}'
+    action-buttons='\${JSON.stringify(args.actionButtons)}'
+    tags='\${JSON.stringify(args.tags)}'
+    download-url="\${args.downloadUrl}"
+    edit-url="\${args.editUrl}"
+    downloads='\${JSON.stringify(args.downloads)}'
+    metadata='\${JSON.stringify(args.metadata)}'
+    variant="\${args.variant}"
+  ></data-card>\``,...(x=(S=l.parameters)==null?void 0:S.docs)==null?void 0:x.source}}};const C=["Default","Minimal","Detailed","SFB980","SFB980detailed"];export{s as Default,e as Detailed,t as Minimal,i as SFB980,l as SFB980detailed,C as __namedExportsOrder,B as default};
